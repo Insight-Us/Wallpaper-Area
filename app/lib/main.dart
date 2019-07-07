3f34249f799f8eaa1db6c3894300cd6fad6de386
AjjuSingh <ajaykumar.kumar900@gmail.com>
2019-07-07 23:56:19 +0530

    basic ui done

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,64 +1,119 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MaterialApp(
+      home: home(),
+      debugShowCheckedModeBanner: false,
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wallpaper Arena',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Wallpaper Arena'),
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  // AppBar logo image asset
+  Widget getImageAsset() {
+    AssetImage image = AssetImage('assets/logo.png');
+    Image img = Image(
+      image: image,
+      width: 30,
+      height: 30,
+    );
+    return Padding(
+      padding: const EdgeInsets.only(left: 15.0),
+      child: Container(child: img),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  _MyHomePageState({this.data});
-
-  
+  Widget latestView() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Latest',
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) => Container(
+                  margin: EdgeInsets.only(right: 5.0),
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: ClipRRect(
+                      borderRadius: new BorderRadius.circular(8.0),
+                      child: Image.network(
+                        "http://getwallpapers.com/wallpaper/full/5/e/1/674609.jpg",
+                        fit: BoxFit.cover,
+                      )),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white),
+                ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Categories',
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: (ctx, int val) {
+              return Container(
+                margin: EdgeInsets.all(5.0),
+                height: 100,
+                child: Stack(
+                  children: <Widget>[
+                    Center(
+                      child: new Text(
+                        "Category $val",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    )
+                  ],
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        elevation: 0.0,
+        title: new Text("SOFT-AI Wallpaper"),
+        backgroundColor: Colors.black,
         centerTitle: true,
+        leading: getImageAsset(),
       ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 50, top: 10),
-            child: new Row(children: <Widget>[
-              Expanded(child: new Text("Game")),
-              Expanded(child: new Text("Movie")),
-              Expanded(child: new Text("Anime")),
-              Expanded(child: new Text("Sports")),
-            ],
-            crossAxisAlignment: CrossAxisAlignment.center,),
-          ),
-          GridView.count(
-            crossAxisCount: 2,
-            children: <Widget>[
-              
-            ],
-          )
-        ],
-      ), 
+      body: latestView(),
+      backgroundColor: Color(0x1F221010),
     );
   }
 }
