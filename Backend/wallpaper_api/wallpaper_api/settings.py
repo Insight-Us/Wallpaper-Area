b1ef7bda37378935fb1a86270b2bfaf6564ec78f
AjjuSingh <ajaykumar.kumar900@gmail.com>
2019-07-08 23:42:18 +0530

    configuration for heroku

--- Backend/wallpaper_api/wallpaper_api/settings.py
+++ Backend/wallpaper_api/wallpaper_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -68,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+django_heroku.settings(locals())
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
