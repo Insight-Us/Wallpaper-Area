e3d06c423997f93a4b55797980405957dfa12cc3
AjjuSingh <ajaykumar.kumar900@gmail.com>
2019-07-10 18:26:25 +0530

    Added cover_page to category model

--- Backend/wallpaper_api/api_development/models.py
+++ Backend/wallpaper_api/api_development/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=100)
+    cover_page = models.CharField(max_length=150, null=True)
 
     def __str__(self):
         return self.category_name
