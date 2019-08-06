b7fd4e8afb7c9942d3be0ca00c5b5141acb99342
AjjuSingh <ajaykumar.kumar900@gmail.com>
2019-08-06 20:47:10 +0530

    Version 3.0 alpha release

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -19,43 +19,24 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Container(
-
-              child: new RichText(
-            text: TextSpan(children: [TextSpan(text: 'C', style: TextStyle(fontSize: 40.0)), TextSpan(text: 'ategory', style: TextStyle(fontSize: 30.0)), ]),
-          )),
-          backgroundColor: Colors.pink[700].withOpacity(1.0),
-        ),
-        body: Stack(children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Image(
-              image:
-                  AssetImage("assets/Colorful-App-Tiles-Android-Wallpaper.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          /*Align(
-            alignment: Alignment.topCenter,
-            child: new RichText(
-              text: TextSpan(children: [
-                TextSpan(
-                    text: "C",
-                    style: TextStyle(fontSize: 45.0, fontFamily: "Ubuntu")),
-                TextSpan(
-                    text: "ategories",
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontFamily: "Ubuntu",
-                      fontWeight: FontWeight.normal
-                    ))
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Stack(children: [
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image(
+                    image: AssetImage(
+                        "assets/Colorful-App-Tiles-Android-Wallpaper.gif"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                WallpaperCard()
               ]),
             ),
-          ),*/
-          WallpaperCard()
-        ]),
+          ],
+        ),
       ),
     );
   }
--- app/lib/widgets/wallpaperCard.dart
+++ app/lib/widgets/wallpaperCard.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'selectionArea.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:wallpaper/wallpaper.dart' as wall;
+import 'dart:async';
+import 'package:app/utility/wallpaperSetter.dart';
 
 class WallpaperCard extends StatefulWidget {
   @override
@@ -42,72 +46,141 @@ class _WallpaperCardState extends State<WallpaperCard>
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: getJsonData(),
-      builder: (BuildContext ctx, AsyncSnapshot snap) => data == null
-          ? Align(
-              alignment: Alignment.center,
-              child: CupertinoActivityIndicator())
-          : ListView.builder(
-              itemCount: Category.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding:
-                      const EdgeInsets.only(top: 5.0, left: 10.0, right: 10.0),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) => SelectionArea(
-                                    categorySelected: Category[index]
-                                        ["category_name"],
-                                    url: Category[index]["cover_page"],
-                                    collection: data,
-                                    id: Category[index]["id"],
-                                  )));
-                    },
-                    child: SizedBox(
-                      height: 100,
-                      child: Hero(
-                        tag: Category[index]["category_name"].toString(),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: new Text(
+            "Just arrived",
+            style: TextStyle(
+                fontSize: 30.0, fontFamily: 'Kaushan', color: Colors.white),
+          ),
+        ),
+        Expanded(
+          child: data == null
+              ? CupertinoActivityIndicator()
+              : new Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    WallpaperSetter(url: data[index]["image"],)
+                                    ));
+                      },
+                      onLongPress: () async {
+                        await wall.Wallpaper.homeScreen(data[index]["image"]);
+                        print(data[index]["image"]);
+                      },
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
                         child: Card(
-                          clipBehavior: Clip.antiAlias,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5.0)),
                           elevation: 3.0,
+                          clipBehavior: Clip.antiAlias,
                           color: Colors.black,
-                          child: Stack(
-                              alignment: Alignment.bottomCenter,
-                              fit: StackFit.expand,
-                              children: [
-                                new Image.network(
-                                  Category[index]["cover_page"].toString(),
-                                  fit: BoxFit.cover,
-                                  color: Colors.black45,
-                                  colorBlendMode: BlendMode.darken,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 18.0),
-                                  child: Align(
-                                      alignment: Alignment.center,
-                                      child: new Text(
-                                        Category[index]["category_name"],
-                                        style: TextStyle(
-                                          fontFamily: "Kaushan",
-                                          color: Colors.white,
-                                          fontSize: 30.0,
-                                        ),
-                                      )),
-                                ),
-                              ]),
+                          child: FadeInImage(
+                            placeholder: AssetImage("assets/loading.gif"),
+                            image : new NetworkImage(
+                              "https://vivrti.pythonanywhere.com" +
+                                  data[index]["link_thumbnail"],
+                            ),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
+                    );
+                  },
+                  itemCount: 10,
+                  viewportFraction: 0.6,
+                  scale: 0.8,
+                ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: new Text(
+            "Category",
+            style: TextStyle(
+                fontSize: 30.0, fontFamily: 'Kaushan', color: Colors.white),
+          ),
+        ),
+        Expanded(
+          child: FutureBuilder(
+            future: getJsonData(),
+            builder: (BuildContext ctx, AsyncSnapshot snap) => data == null
+                ? Align(
+                    alignment: Alignment.center,
+                    child: CupertinoActivityIndicator())
+                : ListView.builder(
+                    itemCount: Category.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                            top: 5.0, left: 10.0, right: 10.0),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        SelectionArea(
+                                          categorySelected: Category[index]
+                                              ["category_name"],
+                                          url: Category[index]["cover_page"],
+                                          collection: data,
+                                          id: Category[index]["id"],
+                                        )));
+                          },
+                          child: Container(
+                            height: 80,
+                            child: Hero(
+                              tag: Category[index]["category_name"].toString(),
+                              child: Card(
+                                clipBehavior: Clip.antiAlias,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0)),
+                                elevation: 3.0,
+                                color: Colors.black,
+                                child: Stack(
+                                    alignment: Alignment.bottomCenter,
+                                    fit: StackFit.expand,
+                                    children: [
+                                      new Image.network(
+                                        Category[index]["cover_page"]
+                                            .toString(),
+                                        fit: BoxFit.cover,
+                                        color: Colors.black45,
+                                        colorBlendMode: BlendMode.darken,
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 18.0),
+                                        child: Align(
+                                            alignment: Alignment.center,
+                                            child: new Text(
+                                              Category[index]["category_name"],
+                                              style: TextStyle(
+                                                fontFamily: "Kaushan",
+                                                color: Colors.white,
+                                                fontSize: 30.0,
+                                              ),
+                                            )),
+                                      ),
+                                    ]),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
+          ),
+        ),
+      ],
     );
   }
 }
